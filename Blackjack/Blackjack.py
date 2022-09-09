92a36cf8612bfcd3e178626a6ec34bea1b144eff
Erik Chan <chanerik1000@gmail.com>
2022-09-08 21:19:26 -0700

    Added Multiple Players and Player Class

--- Blackjack/Blackjack.py
+++ Blackjack/Blackjack.py
@@ -19,23 +19,15 @@ class Card:
     def count(self):
         return self.count
 
-
-
-"""if Ace:
-    default = 11
-    if total > 21:
-        total -= 10
-"""
-
 class Deck:
     def __init__(self):
         SUITS = ['Hearts', 'Diamonds', 'Spades', 'Clubs']
         VALUES = ['Ace', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King']
-        Count = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+        Count = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
         self.cards = deque()
         for suit in SUITS:
-            for value in VALUES:
-                self.cards.append(Card(suit, value))
+            for value in range(len(VALUES)):
+                self.cards.append(Card(suit, VALUES[value], Count[value]))
 
     def size(self):
         return len(self.cards)
@@ -71,83 +63,158 @@ class Deck:
         self.shuffle()
 
 class Player:
-    def __init__(self, cards):
-        self.cards = cards
+    def __init__(self):
+        self.cards = deque()
 
 
     def add(self, card):
-        self.card.append(card)
+        self.cards.append(card)
+
+    def getCards(self):
+        return self.cards
+
+    def reset(self):
+        self.cards = deque()
+
+    def length(self):
+        return len(self.cards)
 
 
 class BlackJack:
-    def __init__(self, numOfPlayers):
+    def __init__(self, numOfPlayers = 1):
         self.deck = Deck()
         self.deck.shuffle()
-        self.player = deque()
         self.dealer = deque()
+        self.players = []
 
-        self.players = [Player() for x in range(numOfPlayers)]
+        for x in range(numOfPlayers):
+            self.players.append(Player())
 
-    def _get_score(self):
+    def getAll(self):
+        for x in range(len(self.players)):
+            dealer = self._get_score()[1]
+            player = self._get_score(x)[0]
+
+            print(f"Dealer: {dealer}")
+            print(f"Player {x}: {player}")
+
+        print("\n")
+
+    def _get_score(self, playerNum = 0):
         player_score = 0
         dealer_score = 0
-        for card in self.player:
+
+        playerAces = 0
+        dealerAces = 0 
+
+        for card in self.players[playerNum].getCards():
             if card.value == 'Ace':
-                player_score += 11
-            elif card.value in ['Jack', 'Queen', 'King']:
-                player_score += 10
-            else:
-                player_score += card.count()
+                playerAces += 1
+
+            player_score += card.count
+
+            if player_score > 21 and playerAces:
+                player_score -= 10
+                playerAces -= 1
 
         for card in self.dealer:
             if card.value == 'Ace':
-                dealer_score += 11
-            elif card.value in ['Jack', 'Queen', 'King']:
-                dealer_score += 10
-            else:
-                dealer_score += card.count()
+                dealerAces += 1
+
+            dealer_score += card.count
+
+            if dealer_score > 21 and dealerAces:
+                dealer_score -= 10
+                dealerAces -= 1
 
         return player_score, dealer_score
 
+    def getResults(self):
+        dealerScore = self._get_score()[0]
 
-    def _print_current_hand(self):
-        print("Player's hand:")
-        for card in self.player:
+        print(f"Dealer Sccore: {dealerScore}")
+        for x in range(len(self.players)):
+            playerScore = self._get_score(x)[1]
+
+            if playerScore > 21:
+                playerScore = "Busted"
+
+            elif playerScore == 21 and self.players[x].length() == 2:
+                playerScore = "Blackjack!"
+
+            print(f"Player {x} Score: {playerScore}")
+
+        print("\n")
+
+
+    def _print_current_hand(self, num = 0):
+        print(f"Player {num} hand:")
+        for card in self.players[num].getCards():
             print(card)
 
         print("Dealer's hand:")
         for card in self.dealer:
             print(card)
 
-    def deal_new_hand(self, numPlayers):
-        for x in range(numPlayers):
-            self.player.append(self.deck.draw())
-            self.player.append(self.deck.draw())
+        print("\n")
+
+    def deal_new_hand(self):
+        for x in range(len(self.players)):
+            self.players[x].add(self.deck.draw())
+            self.players[x].add(self.deck.draw())
+        
         self.dealer.append(self.deck.draw())
         self.dealer.append(self.deck.draw())
 
 
     
-    def hit(self):
+    def hit(self, playerNum = 0):
         if self.deck.size() > 0:
-            #self.player.append(self.deck.draw())
-            
-            myPlayer.add(self.deck.draw)
-            self._print_current_hand()
-            self._get_score()
+            self.players[playerNum].add(self.deck.draw())
+            self._print_current_hand(playerNum)
+            self._get_score(playerNum)
+
+            if self._get_score(playerNum)[0] == 21 and self.players[playerNum].length() == 2:
+                print(f"BlackJack! Player {playerNum} wins 3/2")
+
+            if self._get_score(playerNum)[0] > 21:
+                print(f"Player {playerNum} Busts.")
 
         else:
             raise Exception("Deck is empty")
 
+        print("\n")
+
+    def dealerHit(self):
+        if self.deck.size() > 0:
+            self.dealer.append(self.deck.draw())
+
+    def Pass(self, playerNum = 0):
+        while self._get_score()[1] < 17:
+            self.dealerHit()
+
+        print(self._get_score()[1])
+
+        if self._get_score()[1] > 21:
+            print("Dealer Bust, Player Wins")
+
+        print("\n")
+
     def reshuffle(self):
         self.deck.reset()
-        self.player = deque()
+        for x in self.players:
+            x.reset()
+
         self.dealer = deque()
 
-myGame = BlackJack()
+myGame = BlackJack(4)
 
 myGame.deal_new_hand()
 
+myGame.getAll()
+
 myGame.hit()
 
-    
+myGame._print_current_hand(1)
+
+myGame._print_current_hand(3)
