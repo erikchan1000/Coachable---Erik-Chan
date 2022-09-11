fb71487f87846c40e6c21c43517286b2b82158f6
Erik Chan <chanerik1000@gmail.com>
2022-09-11 15:24:56 -0700

    fixed quicksort

--- Sorting/QuickSort.py
+++ Sorting/QuickSort.py
@@ -1,53 +1,50 @@
 import random
 
-#Self Implementation
-
-def quickSort(array, lo, hi):
-    if lo >= hi:
+def quickSort(array, start, end):
+    if start >= end:
         return
 
-    pivotIndex = random.randint(lo, hi)
+    pivotIndex = random.randrange(start, end + 1)
+
+    print(f"Pivot Index : {pivotIndex}")
+
 
     pivot = array[pivotIndex]
 
-    #Shifts pivot to front
-    array[lo], array[pivotIndex] = pivot, array[lo]
+    array[start], array[pivotIndex] = pivot, array[start]
 
-    p1 = lo + 1
+    print(array)
 
-    p2 = hi
+    p1 = start + 1
+
+    p2 = end
 
     while p1 <= p2:
-        if array[p1] >= pivot and array[p2] < pivot:
+
+        if array[p1] > pivot and array[p2] <= pivot:
             array[p1], array[p2] = array[p2], array[p1]
+
             p1 += 1
             p2 -= 1
 
-        elif array[p1] >= pivot:
+        elif array[p1] > pivot:
             p2 -= 1
 
-        elif array[p2] < pivot:
+        elif array[p2] <= pivot:
             p1 += 1
 
         else:
             p1 += 1
             p2 -= 1
 
-    array[0], array[p1 - 1] = array[p1 - 1], array[0]
-
-    print("Pivot Index: " + str(pivot))
-
-    print(array)
-
-    quickSort(array, lo, p1 - 1)
-    quickSort(array, p1, hi)
+    array[p2], array[start] = array[start], array[p2]
 
 
+    quickSort(array, start, p2 - 1)
+    quickSort(array, p2 + 1, end)
 
+arr = [2,0,2,1,1,0]
 
-if __name__ == "__main__":
-    arr = [2,1]
+quickSort(arr, 0, len(arr) - 1)
 
-    quickSort(arr, 0, len(arr) - 1)
-
-
+print(arr)
