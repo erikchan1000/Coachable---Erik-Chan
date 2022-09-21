144163cec4ae4d76495c214c90ebd5a2e684583c
Erik Chan <chanerik1000@gmail.com>
2022-09-20 21:14:25 -0700

    testing for init of collinear point finder

--- Finding Collinears Points/Collinear.py
+++ Finding Collinears Points/Collinear.py
@@ -1,3 +1,6 @@
+from re import L
+
+
 class Point:
   # Initializes 2D point with x,y coordinate/
   def __init__(self, x: float, y: float):
@@ -5,7 +8,7 @@ class Point:
     self.y = y
     
   # Built in comparator sorting Points by y-coordinate first and using x-coordinate for ties.
-  def __cmp__(self, other: Point) -> bool:
+  def __cmp__(self, other) -> bool:
     if self.y == other.y:
         return self if self.x < other.x else other
 
@@ -17,7 +20,7 @@ class Point:
   # Horizontal lines have 0 slope.
   # Veritcal lines have infinity slope.
   # If self == other, then return negative infinity.
-  def slope_to(self, other: Point) -> float:
+  def slope_to(self, other) -> float:
     if self == other:
         return float("-inf")
     
@@ -35,17 +38,73 @@ class CollinearPointFinder:
   # This method should have O(N^2 log N) runtime.
   # Hint: This method will be the majority of the logic in your code
   # It will include a lot of preprocessing to make the other methods very quick.
-  def __init__(self, points: List[Point]):
+  def __init__(self, points: list[Point]):
+    hmap = {}
     
-  
+    for p in range(len(points)):
+        temp = points[:p] + points[p + 1 :]
+
+        temp.sort(key = lambda x : points[p].slope_to(x))
+        
+        p1 = 0
+
+        p2 = 0
+
+        while p1 < len(temp):
+            slope = temp[p1].slope_to(points[p])
+            if p2 != p1:
+                
+                slope2 = temp[p2].slope_to(points[p])
+
+                if slope == slope2 and p2 < len(points) - 1:
+                    p2 += 1
+
+                else:
+                    p1 += 1
+
+            elif p2 < len(temp) - 1:
+                p2 += 1
+
+            else: 
+                p1 += 1
+
+            if p2 - p1 + 2 not in hmap:
+                hmap[p2 - p1 + 2] = [[points[p]] + temp[p1 : p2 + 1]]
+
+            else:
+                hmap[p2 - p1 + 2].append([points[p]] + temp[p1 : p2 + 1])
+
+    for x in hmap:
+        for i in hmap[x]:
+            for z in range(len(i)):
+                i[z] = (i[z].x, i[z].y)
+
+    for x in hmap:
+        print(x)
+
+        print(hmap[x])
+
+
   # Returns the number of points in CollinearPointFinder
   # The runtime should be O(1)
   def size(self) -> int:
-    
+    pass
   # Returns the number of collinear segments with at least length k.
   # The runtime should be O(1)
   def num_segments(self, k: int) -> int:
+    pass
   
   # Returns a list of all collinear segments with at least length k.
   # The runtime should be O(1)
   def get_segments(self, k: int) -> int:
+    pass
+
+
+
+
+points = [(1, 2), (3, 9), (2, 3), (4, 5), (2, 6), (3, 4), (1, 3), (10, 3), (4, 12)]
+
+for x in range(len(points)):
+    points[x] = Point(points[x][0], points[x][1])
+
+test = CollinearPointFinder(points)
